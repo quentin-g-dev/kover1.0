7b6e319728adf97e294ad9ecf28971c64f16ae45
quentin-g-dev <qgermain@gmail.com>
2020-10-15 09:14:13 +0200

    fichiers à jour

--- php/parts/kover_steps/choice_step.php
+++ php/parts/kover_steps/choice_step.php
@@ -1,17 +1,16 @@
 <!--MAKE YOUR CHOICE-->
-<div id="sourceChoice" class="d-none">
-    <div class="d-flex flex-column flex-lg-column mx-auto my-3">
-        <button class="btn bg-kover text-white font-weight-bold  p-3 mw-75 rounded mx-auto mx-lg-5 mt-4 h3 p-2"
-            id="newTextButton">
+<main
+    class="container full-container  d-flex flex-column flex-no-wrap flex-lg-wrap p-3 mx-auto mt-3 mb-2 text-center w-100 w-lg-75 justify-content-around align-items-center h-100 rounded"
+    id="choice">
+    <h2 class="text-info mx-auto h2 my-3 font-weight-bold">
+        Sélectionnez une option
+    </h2>
+    <div class="d-flex flex-column flex-lg-row mr-auto ml-auto my-3">
+        <button class="btn btn-info w-100 w-lg-75 rounded mx-auto mx-lg-5 mt-4 h3 p-2" id="newTextButton">
             Saisir/coller une lettre de motivation
         </button>
-        <button class="btn bg-kover text-white font-weight-bold p-3 mw-75 rounded mx-auto mx-lg-5 mt-4 h3 p-2"
-            id="uploadFileButton">
+        <button class="btn btn-info w-100 w-lg-75 rounded mx-auto mx-lg-5 mt-4 h3 p-2" id="uploadFileButton">
             Utiliser un fichier existant
         </button>
-        <button class="btn bg-kover text-white font-weight-bold p-3 mw-75 rounded mx-auto mx-lg-5 mt-4 h3 p-2"
-            id="adaptATemplateButton">
-            Adapter un modèle
-        </button>
     </div>
-</div>
+</main>
